1cf5f715ffd2d30520c177c79a570474bb606547
obiorbitalstar <obiorbitalstar@gmail.com>
2020-08-22 18:03:59 +0300

    added tests and modified the function

--- tests/challenges/test_array_shift.py
+++ tests/challenges/test_array_shift.py
@@ -1,17 +1,29 @@
-from data_structures_and_algorithms.challenges.array_shift.array_shift import insertShiftArray
+from data_structures_and_algorithms.challenges.array_shift.array_shift import insert_shift_array
 
 def test_arr_shift():
-   actual = insertShiftArray([2,4,6,8],5)
+   actual = insert_shift_array([2,4,6,8],5)
    expected = [2,4,5,6,8]
    assert actual == expected
 
 def test_arr_shift2():
-    actual = insertShiftArray([4,8,15,23,42],16)
+    actual = insert_shift_array([4,8,15,23,42],16)
     expected = [4,8,15,16,23,42]
     assert actual == expected
 
 def test_arr_shift3():
-    actual= insertShiftArray([1,5,10,15],20)
+    actual= insert_shift_array([1,5,10,15],20)
     expected = [20,1,5,10,15]
     assert actual != expected
 
+def test_none_value():
+    actual = insert_shift_array([1,2,3,4,5],None)
+    expected = [1,2,3,4,5]
+    assert expected == actual
+def test_empty_array():
+    actual = insert_shift_array([],5)
+    expected = [5]
+    assert actual == expected
+def test_empty_none():
+    actual = insert_shift_array([],None)
+    expected = []
+    assert actual == expected
