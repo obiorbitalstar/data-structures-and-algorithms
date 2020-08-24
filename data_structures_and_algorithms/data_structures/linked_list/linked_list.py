8f635f9135501466638b65d0aa907f2a60668889
obiorbitalstar <obiorbitalstar@gmail.com>
2020-08-24 03:15:28 +0300

    code challenge 06 is done

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,126 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
     # put your LinkedList implementation here
-    pass
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        elements = []
+        current = self.head
+        while current.next != None:
+            current = current.next
+            elements.append(current.data)
+        print(''.join("{} -> ".format(*k) for k in enumerate(elements))+'NULL')
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
+
+    def appened(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    def insert_before(self,next_node,data):
+
+        if not next_node:
+            print('node dose not exist')
+            return
+        new_node=Node(data)
+        current_node=self.head
+        if next_node == current_node:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        while current_node:
+            if current_node.next==next_node:
+                new_node.next=current_node.next
+                current_node.next=new_node
+                return
+            else:
+                current_node=current_node.next
+
+    def insert_after(self, prev_node,data):
+        if not prev_node:
+            print('The previous node is not in the list')
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next=new_node
+
+
+    def length(self):
+        current = self.head
+        total = 0
+
+        while current:
+            total += 1
+            current = current.next
+        return total
+
+    def includes(self, value):
+        current_node = self.head
+        while current_node.next != None:
+            if current_node.data == value:
+                return True
+            else:
+                current_node = current_node.next
+        return False
+    def get(self,index):
+        if index == self.length():
+            raise Exception("Index is out of range")
+            return None
+        current_index = 0
+        current_node =self.head
+        while True:
+            current_node=current_node.next
+            if current_index == index:return current_node.data
+            current_index+=1
+
+    def erase(self, index):
+        if index >= self.length():
+            raise Exception("this index is out of range")
+            return None
+        current_index = 0
+        current_node = self.head
+        while True:
+            last_node = current_node
+            current_node = current_node.next
+            if current_index == index:
+                last_node.next = current_node.next
+                return
+            current_index += 1
+
+
+if __name__ == "__main__":
+    my_list = LinkedList()
+    my_list.appened(1)
+    my_list.appened(2)
+    my_list.appened(3)
+    my_list.appened(4)
+    my_list.appened(5)
+    print(my_list.display())
+    y= my_list.head.next.next.next
+    my_list.insert_after(y,6)
+    print(my_list.display())
+    my_list.insert_before(my_list.head.next.next.next,'a')
+    print(my_list.display())
--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,116 @@
-from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,
-)
+from data_structures_and_algorithms.data_structures.linked_list.linked_list import LinkedList
+import pytest
 
 
-def test_instance():
-    ll = LinkedList()
-    assert isinstance(ll, LinkedList)
+@pytest.fixture
+def data():
+    linked_list = LinkedList()
+    return {'list': linked_list}
+
+
+def test_empty(data):
+    actual = data['list'].length()
+    expected = 0
+    assert actual == expected
+
+
+def test_insert(data):
+    actual = data['list'].appened(1)
+    expected = 1
+    assert expected == data['list'].length()
+
+
+def test_head(data):
+    # The .get is a function i made to get the value given an index ,at index 0 is the head , i entered the first value and checked index of 0
+    head = data['list'].appened('firstEntry')
+    expected = 'firstEntry'
+    assert expected == data['list'].head.data
+
+def test_multi_input(data):
+    # length is a method i made to check the length of a linked list
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(3)
+    data['list'].appened(4)
+    actual = data['list'].length()
+    expected = 4
+    assert actual == expected
+
+
+def test_finiding_value(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(3)
+    actual = data['list'].includes(2)
+    expected = True
+    assert expected == actual
+
+
+def test_dont_exists(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    actual = data['list'].includes(5)
+    expected = False
+    assert expected == actual
+
+
+def test_print_all(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(3)
+    data['list'].appened(4)
+    data['list'].appened(5)
+    expected = [1, 2, 3, 4, 5]
+    actual = data['list'].display()
+    assert expected == actual
+
+def test_add_to_end_and_add_multi_to_end(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(3)
+    data['list'].appened(4)
+    data['list'].appened(5)
+    actual = data['list'].display()
+    expected = [1,2,3,4,5]
+def test_add_to_middle(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(4)
+    data['list'].appened(5)
+    data['list'].appened(6)
+    location = data['list'].head.next.next
+    data['list'].insert_before(location,3)
+    actual = data['list'].display()
+    expected = [1,2,3,4,5,6]
+    assert expected==actual
+def test_add_to_first(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    location = data['list'].head
+    data['list'].insert_before(location,5)
+    actual = data['list'].display()
+    expected = [5,1,2]
+    assert expected == actual
+
+def test_insert_after_middle(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    data['list'].appened(3)
+    data['list'].appened(5)
+    data['list'].appened(6)
+
+    location = data['list'].head.next.next
+    data['list'].insert_after(location,4)
+
+    actual = data['list'].display()
+    expected = [1,2,3,4,5,6]
+    assert expected ==actual
+
+def test_insert_after_last_node(data):
+    data['list'].appened(1)
+    data['list'].appened(2)
+    location = data['list'].head.next
+    data['list'].insert_after(location,3)
+    actual = data['list'].display()
+    expected = [1,2,3]
+    assert expected == actual
