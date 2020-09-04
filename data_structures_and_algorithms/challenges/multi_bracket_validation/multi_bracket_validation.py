0bd821d7c25ce69c3a2cdd6f8bd51302591ec816
obiorbitalstar <obiorbitalstar@gmail.com>
2020-09-04 12:49:57 +0300

    multi

--- data_structures_and_algorithms/challenges/multi_bracket_validation/multi_bracket_validation.py
+++ data_structures_and_algorithms/challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -1,12 +1,12 @@
 def multi_bracket_validation(input):
-    curly_1=input.count('{')
-    curly_2=input.count('}')
-    paran_1=input.count('(')
-    paran_2=input.count(')')
-    brack_1=input.count('[')
-    brack_2=input.count(']')
-    print(brack_1,paran_1)
-    if curly_1 == 0 and curly_2 ==0  and paran_1==0 and paran_2==0 and brack_1 ==0 and brack_2 ==0:
+    curly_1 = input.count('{')
+    curly_2 = input.count('}')
+    paran_1 = input.count('(')
+    paran_2 = input.count(')')
+    brack_1 = input.count('[')
+    brack_2 = input.count(']')
+    print(brack_1, paran_1)
+    if curly_1 == 0 and curly_2 == 0 and paran_1 == 0 and paran_2 == 0 and brack_1 == 0 and brack_2 == 0:
         return False
     elif curly_1 != curly_2:
         return False
@@ -16,5 +16,6 @@ def multi_bracket_validation(input):
         return False
     else:
         return True
+
 
 print(multi_bracket_validation('[this is() a{ test for true]'))
